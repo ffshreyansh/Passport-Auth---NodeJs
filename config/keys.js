2fa1dcbe7185afa41cefa2352ae49b8b7e4e8136
Shreyansh Kumar <shreyanshkumar058@gmail.com>
2022-12-28 19:50:12 +0530

    MongoDb Database Connected

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: "mongodb+srv://shreyanshkr:20bce10804@passportauth.wgct7vf.mongodb.net/?retryWrites=true&w=majority    "
+}
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,45 @@ const express  = require("express");
 const router = express.Router();
 
 //Login Page
-router.get('/login', (req, res) => res.send('Login'));
+router.get('/login', (req, res) => res.render('login'));
 
 
 //Register Page
-router.get('/register', (req, res) => res.send('Register'));
+router.get('/register', (req, res) => res.render('register'));
+
+router.post("/register", (req, res) => {
+    const {name, email, password, password2} = req.body
+    let errors = []
+
+    //check required fields
+    if(!name || !email || !password || !password2){
+        errors.push({msg: "please fill all fields"  });
+
+    }
+
+    //check passwords match
+    if(password !== password2){
+        errors.push({msg: "Password don't match"});
+    }
+
+    //check password length
+    if(password.length < 6){
+        errors.push({msg: "password must be greater than 6 characters"})
+    }
+
+    if(errors.length > 0)
+    {
+        res.render("register",{
+            errors,
+            name,
+            email,
+            password,
+            password2
+        });
+    }else {
+        res.send("pass")
+    }
+});
 
 
 module.exports = router;
